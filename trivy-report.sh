71a743d8a1efcafafe70beda00d54fd91584b42f
ummehabiba-bjit <142980788+ummehabiba-bjit@users.noreply.github.com>
2023-10-04 16:50:21 +0600

    Update trivy-report.sh

--- trivy-report.sh
+++ trivy-report.sh
@@ -3,11 +3,10 @@
 # Read the Trivy report from the file
 trivy_report=$(cat trivy_scan.log)
 
-# Define your Slack webhook URL
-slack_webhook_url=https://hooks.slack.com/services/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
-
+# Define your Slack webhook UR
+SLACK_WEBHOOK= slack-webhook-url
 # Create a JSON payload for the Slack message
 payload="{\"text\":\"Trivy Vulnerability Report:\\n$trivy_report\"}"
 
 # Send the message to Slack
-curl -X POST -H 'Content-type: application/json' --data "$payload" "$slack_webhook_url"
+curl -X POST -H 'Content-type: application/json' --data "$payload" "$SLACK_WEBHOOK"
