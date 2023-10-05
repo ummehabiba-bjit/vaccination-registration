5e3bfda490c10abaf775c875592dffa344d3109e
ummehabiba-bjit <142980788+ummehabiba-bjit@users.noreply.github.com>
2023-10-05 08:41:56 +0600

    Update trivy-report.sh

--- trivy-report.sh
+++ trivy-report.sh
@@ -6,12 +6,13 @@ trivy_report=$(cat trivy_scan.log)
 # Retrieve the Slack webhook URL from AWS Systems Manager Parameter Store
 SLACK_WEBHOOK=$(aws ssm get-parameter --name slack-webhook-url --with-decryption --query "Parameter.Value" --output text)
 
-# Create a JSON payload for the Slack message
-payload=
-{
-  "text": "Trivy Vulnerability Report:\\n$trivy_report"
-}
-# "{\"text\":\"Trivy Vulnerability Report:\\n$trivy_report\"}"
+# # Create a JSON payload for the Slack message
+# payload=
+# {
+#   "text": "Trivy Vulnerability Report:\\n$trivy_report"
+# }
+# # "{\"text\":\"Trivy Vulnerability Report:\\n$trivy_report\"}"
 
-# Send the message to Slack
-curl -X POST -H 'Content-type: application/json' --data "$payload" "$SLACK_WEBHOOK"
+# # Send the message to Slack
+# curl -X POST -H 'Content-type: application/json' --data "$payload" "$SLACK_WEBHOOK"
+curl -X POST -H 'Content-type: application/json' --data '{"text": "Trivy Vulnerability Report:\n$(cat trivy_scan.log)"}' $SLACK_WEBHOOK
